4131225de8b7a84ac6c16c3e106759d593e03d23
ChoiJaeLyang <cjl2076@naver.com>
2020-09-05 23:29:42 +0900

    주석 추가

--- src/main/java/com/hibuddy/springboot/web/UserApiController.java
+++ src/main/java/com/hibuddy/springboot/web/UserApiController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserApiController {
     private final UserService userService;
 
+    //아이디 중복 확인
     @PostMapping("/api/user/info") //조회(R)
     public String confirmUserId(@RequestBody String userId){
         if(userService.confirmUserId(userId))
@@ -18,11 +19,13 @@ public class UserApiController {
             return "available";
     }
 
+    //회원 가입
     @PostMapping("/api/user/join") //생성(C)
     public String save(@RequestBody UserRequestDto requestDto){
         return userService.save(requestDto);
     }
 
+    //회원 정보 수정
     @PutMapping("/api/user/update/{userId}")//수정(U)
     public String update(@PathVariable String userId, @RequestBody UserRequestDto userRequestDto){
         return userService.update(userId, userRequestDto);
